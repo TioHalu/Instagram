742b1bd4589dc2efb230fe7c5a76963431429d8e
daffajar <daffafajar45@gmail.com>
2021-12-25 10:56:18 +0700

    menambahkan reels

---
+++ src/components/screenComponents/Database.js
@@ -0,0 +1,36 @@
+//Videos Data
+
+export const videoData = [
+    {
+        video: require('../../storage/Video/Video1.mp4'),
+        postProfile: require('../../storage/images/post1.jpg'),
+        title: 'Daffajar',
+        description: 'Sakit?? NO, Malu?? YES!!!',
+        likes:'3511k',
+        isLike: false
+    },
+    {
+        video: require('../../storage/Video/Video2.mp4'),
+        postProfile: require('../../storage/images/post2.jpg'),
+        title: 'Agustio',
+        description: 'Futsal dulu baru kamu :p',
+        likes:'13513851351684684k',
+        isLike: false
+    },
+    {
+        video: require('../../storage/Video/Video3.mp4'),
+        postProfile: require('../../storage/images/post3.jpg'),
+        title: 'Bintang',
+        description: 'Nightmare!',
+        likes:'3251k',
+        isLike: false
+    },
+    {
+        video: require('../../storage/Video/Video4.mp4'),
+        postProfile: require('../../storage/images/post4.jpg'),
+        title: 'Harry',
+        description: 'Apaan tu mann!!!',
+        likes:'2184k',
+        isLike: false
+    }
+]
